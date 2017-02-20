82ab899b2a7c791eef9ee8bba636b392b99ab22e
b-insh <bethanyhyland29@gmail.com>
2017-02-20 14:29:23 -0500

    Check to see if empty and selected cell are in the same row

--- puzzle.js
+++ puzzle.js
@@ -1,21 +1,61 @@
 (function() {
   let puzzle = document.getElementById('puzzle');
-  let gameSize = 15;
+  let gameSize = 15; // 8-, 15-, and 24-puzzle available (easily expanded to larger games (though they will be essentially impossible))
   createGame(gameSize);
-  console.log(gameSize);
-
 
   function createGame(size) {
-    for (let i = 1; i <= size; i++) {
+    for (let i = 1; i <= size + 1; i++) {
       let cell = document.createElement('div');
-      cell.innerHTML = i;
+      if (i <= size) {
+        cell.innerHTML = i;
+        cell.id = i;
+        cell.addEventListener('click', (e) => canMove(e.target.id), false);
+      } else {
+        cell.innerHTML = "";
+        cell.id = 16;
+        cell.classList.add('empty');
+      }
       puzzle.appendChild(cell);
     }
+  }
 
-    let emptyCell = document.createElement('div');
-    emptyCell.innerHTML = "empty";
-    emptyCell.classList.add('empty');
-    puzzle.appendChild(emptyCell);
+  function slideCells() {
+
+
+  }
+
+  function canMove(selectedCell) {
+    console.log(inRow(selectedCell));
+    // if (inRow(selectedCell) || inColumn(selectedCell)) {
+    //   slideCells();
+    // }
+  }
+
+  function inRow(selected) {
+    let empty = document.querySelector('.empty').id;
+    if (empty == selected) return false;
+
+    let numRows = Math.sqrt(gameSize + 1);
+
+    for (let row = numRows; row <= gameSize + 1; row += numRows) {
+      let rowBeginning = row - numRows + 1;
+        if ((rowBeginning <= selected && selected <= row) && (rowBeginning <= empty && empty <= row )) {
+          return true;
+        }
+    }
+    return false;
+  }
+
+  function inColumn(selected) {
+    let empty = document.querySelector('.empty').id;
+    if (empty == selected) return false;
+
+
+
+  }
+
+  function emptycell() {
+    return document.querySelector('.empty').id;
   }
 
 }());
